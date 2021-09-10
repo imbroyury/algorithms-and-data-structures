592c42996f210002772eb45c255d1edc804209df
Yury Imbro <imbroyury@gmail.com>
2021-09-10 12:30:53 +0300

    binary search + tests

--- src/algorithms/binary-search/index.test.ts
+++ src/algorithms/binary-search/index.test.ts
@@ -1,9 +1,21 @@
 import { expect } from 'chai';
 import { binarySearch } from '.';
+import faker from 'faker';
 
 describe('binary search', () => {
-    it('always returns -1', () => {
-        const result = binarySearch([1, 2, 3], 1);
-        expect(result).equal(-1);
+    it('works as native [].indexOf() on random sets of data', () => {
+        for (let i = 0; i < 100; i++) {
+            const array = new Array<number>(100)
+                .fill(0)
+                .map(() => faker.datatype.number())
+                .sort((a, b) => a - b);
+
+            const element =
+                i % 2 === 0
+                    ? faker.datatype.number()
+                    : array[Math.floor(Math.random() * array.length)];
+
+            expect(binarySearch(array, element)).equal(array.indexOf(element));
+        }
     });
 });
--- src/algorithms/binary-search/index.ts
+++ src/algorithms/binary-search/index.ts
@@ -1,3 +1,23 @@
-export const binarySearch = (array: number[], numberToFind: number): number => {
+export const binarySearch = (array: number[], num: number): number => {
+    let low = 0;
+    let high = array.length - 1;
+
+    while (low <= high) {
+        const mid = Math.round((low + high) / 2);
+        const guess = array[mid];
+
+        if (guess === num) {
+            return mid;
+        }
+
+        if (guess < num) {
+            low = mid + 1;
+        }
+
+        if (guess > num) {
+            high = mid - 1;
+        }
+    }
+
     return -1;
 };
